dbb600d3691ce01db0beb256f011c909a12d9ae1
Kevin Jarocki <kjarocki@carfax.ca>
2020-07-15 11:15:09 -0400

    first commit

---
+++ src/test/java/com/carfaxcanada/testingframework/AppiumAppTest.java
@@ -0,0 +1,51 @@
+package com.carfaxcanada.testingframework;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.microsoft.appcenter.appium.Factory;
+import com.microsoft.appcenter.appium.EnhancedAndroidDriver;
+import org.junit.rules.TestWatcher;
+import org.junit.Rule;
+public class AppiumAppTest {
+	
+	@Rule
+	public TestWatcher watcher = Factory.createWatcher();
+	 
+	private static EnhancedAndroidDriver<MobileElement> driver;
+	
+	public static EnhancedAndroidDriver<MobileElement> startApp() throws MalformedURLException {
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+		//capabilities.setCapability(MobileCapabilityType.APP, "C:/Users/kjarocki/Desktop/Maven/swiftnotes.apk"); // Used when testing locally
+		//capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554"); // 
+		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 7913);
+		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+
+		URL url = new URL("http://localhost:4723/wd/hub"); // Used to connect to your Appium Client when testing locally.
+
+		return Factory.createAndroidDriver(url, capabilities);
+	}
+	
+    @Test
+    public void sampleTest() throws MalformedURLException, InterruptedException {
+		
+	  System.out.println("Sample test started.");
+	  driver = startApp();
+	  MobileElement elem = Util.findByByOrName(driver, By.id("com.moonpi.swiftnotes:id/newNote"), "+");
+      elem.click();
+      
+    }
+	@After
+	public void after() throws Exception {
+	      if (driver != null) {
+	            driver.label("Stopping App");
+	            driver.quit();
+	        }
+	}
+}
